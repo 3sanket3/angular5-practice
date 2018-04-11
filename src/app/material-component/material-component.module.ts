050ed01ed1b5042faf49239af2d05633347e9af7
3sanket3 <sanket@redsoftware.in>
2018-04-11 15:11:08 +0530

    devider added

--- src/app/material-component/material-component.module.ts
+++ src/app/material-component/material-component.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule} from '@angular/material';
+import {MatButtonModule, 
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatIconModule} from '@angular/material';
+
+import {MatDividerModule} from "@angular/material/divider"
 
 @NgModule({
   imports: [
-    CommonModule,MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule
+    CommonModule,MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule,MatDividerModule
   ],
-  exports : [MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule],
+  exports : [MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule,MatDividerModule],
   declarations: []
 })
 export class MaterialComponentModule { }
