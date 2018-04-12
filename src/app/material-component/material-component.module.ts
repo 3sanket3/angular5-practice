ed8e95157a0e5969f799e875c344930616be9398
3sanket3 <sanket@redsoftware.in>
2018-04-12 08:41:33 +0530

    nav list added, material prebuilt theme applied

--- src/app/material-component/material-component.module.ts
+++ src/app/material-component/material-component.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import {MatButtonModule, 
   MatCheckboxModule,
   MatSidenavModule,
-  MatIconModule} from '@angular/material';
+  MatIconModule,
+  MatListModule} from '@angular/material';
 
 import {MatDividerModule} from "@angular/material/divider"
 
 @NgModule({
   imports: [
-    CommonModule,MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule,MatDividerModule
+    CommonModule,MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule,MatDividerModule,
+    MatListModule
+
   ],
-  exports : [MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule,MatDividerModule],
+  exports : [MatButtonModule, MatCheckboxModule,MatSidenavModule,MatIconModule,MatDividerModule,
+    MatListModule],
   declarations: []
 })
 export class MaterialComponentModule { }
